d9960ba3516a5240e3c818958712ca1d1d044db6
sabrinara <you@example.com>
2023-10-19 11:29:08 +0600

    Providers Added and modified

---
+++ src/providers/AuthProvider.jsx
@@ -0,0 +1,66 @@
+import { createContext, useEffect, useState } from "react";
+import app from "../../firebase.config";
+import {GoogleAuthProvider, createUserWithEmailAndPassword,onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile, getAuth } from "firebase/auth";
+
+export const AuthContext = createContext(null);
+
+const auth = getAuth(app);
+
+const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    const createUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    const signIn = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+    const googleSignIn = () => {
+        setLoading(true);
+        return signInWithPopup(auth, new GoogleAuthProvider);
+      };
+
+      const handleUpdateProfile = (name, photo) => {
+        return updateProfile(auth.currentUser, {
+            displayName: name, photoURL: photo
+        })
+    }
+
+    const logOut = () => {
+        setLoading(true);
+        return signOut(auth);
+    }
+
+    useEffect(() => {
+        const unSubscribe = onAuthStateChanged(auth, currentUser => {
+            console.log('user in the auth state changed', currentUser);
+            setUser(currentUser);
+            setLoading(false);
+        });
+        return () => {
+            unSubscribe();
+        }
+    }, [])
+
+    const authInfo = {
+        user,
+        loading,
+        createUser,
+        signIn,
+        googleSignIn,
+        handleUpdateProfile,
+        logOut
+    }
+
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
